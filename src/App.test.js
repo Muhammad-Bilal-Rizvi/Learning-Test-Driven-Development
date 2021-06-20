276d5cc90a2aaa87fc523c8d8fc9041e41d2ab45
Muhammad Bilal Rizvi <73776975+Muhammad-Bilal-Rizvi@users.noreply.github.com>
2021-06-20 23:38:51 +0500

    TDD files

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,13 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Hello World/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+
+test('text case', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Hello Piaic/i);
   expect(linkElement).toBeInTheDocument();
 });
